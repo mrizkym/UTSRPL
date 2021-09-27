1a39a85993616a65a36a72956e1b509703a70307
mrizkym <mhmmdrizkymaulana@gmail.com>
2021-09-27 09:43:15 +0800

    HelloWorld

---
+++ HelloWorld.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+  <?php echo "Muhammad Rizky Maulana";?><br>
+  <?php echo "1910817210007";?><br>
+  <?php echo "Hello World, UTS RPL 2021";?><br>
+</body>
+</html>
